f7650a7830e0dca241322c3af3b14c34446de214
Miriam <mgg_pjl@yahoo.com>
2018-08-06 12:27:41 -0500

    Subiendo API key google maps, loader

--- JS/app.js
+++ JS/app.js
@@ -8,3 +8,27 @@
     messagingSenderId: "<SENDER_ID>",
   };
   firebase.initializeApp(config);
+  
+//confi del mapa
+let map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+      infoWindow = new google.maps.InfoWindow; 
+
+
+//Timer for splash
+let showApp;
+
+    function splashIntro() {
+    showApp = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("map").style.display = "block";
+}
+       
