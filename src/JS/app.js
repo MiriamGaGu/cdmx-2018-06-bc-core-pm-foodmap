b5fcd26485ee082887889955d25d2d2a5db535d0
MiriamGaGu <39394044+MiriamGaGu@users.noreply.github.com>
2018-08-06 22:15:57 -0500

    Subiendo coords, welcome, app.js, cambios en el HTML

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,34 +1,50 @@
- // Initialize Firebase
-  // TODO: Replace with your project's customized code snippet
-  let config = {
-    apiKey: "<API_KEY>",
-    authDomain: "<PROJECT_ID>.firebaseapp.com",
-    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-    storageBucket: "<BUCKET>.appspot.com",
-    messagingSenderId: "<SENDER_ID>",
-  };
-  firebase.initializeApp(config);
-  
-//confi del mapa
-let map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
-      
-
-
 //Timer for splash
 let showApp;
 
-    function splashIntro() {
+function splashIntro() {
     showApp = setTimeout(showPage, 3000);
 }
 
 function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("map").style.display = "block";
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("map").style.display = "block";
 }
-       
+
+//confi del mapa, opciones de mapa
+// let map;
+
+addMarker({ coordenadas: { lat: 19.4045352, lng: -99.164021 } });
+addMarker({ coordenadas: { lat: 19.4045352, lng: -99.1662225, 17 } });
+addMarker({ coordenadas: { lat: 19.4045352, lng: -99.1662225, 17 } });
+addMarker({ coordenadas: { lat: 19.4045352, lng: -99.1662225, 17 } });
+addMarker({ coordenadas: { lat: 19.4045352, lng: -99.1662225, 17 } });
+
+const initMap = () => {
+        let options = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -19° 25′ 42, lng: 99° 07′ 39 },
+            zoom: 8
+        });
+
+        const addMarker = (props) => {
+            let marker = new google.map.Marker({
+                    position: props.coordenadas,
+                    map: map
+
+                }
+            }
+        }
+
+        // let mapWeb = new google.map.Maps(document.getElementById('map'), options);
+
+        // //marker en el map, para poner opciones manualmente
+        // let marker = new google.map.Marker({
+        //     position: { lat: 19.4045352, lng: 99.164021 },
+        //     map: map
+        // });
+
+        // let infoWindow = new google.maps.InfoWindow({
+        //     content: '<p>Bar Café Colón</p>'
+        // })
+        // marker.addListener('click', function(){
+        //   infoWindow.open(map, marker);
+        // });
